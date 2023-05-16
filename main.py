e381aee824d8235055e25d23f3866b310aa19f80
Belikov <dimabelka@mail.ru>
2023-05-16 17:53:13 +0500

    Добавил config.py в нем настройки сервера и локальных путей. Добавлена обработка исключений Добавлена прозрачность watermark Добавлена установка изображения на рабочий стол Добавлена проверка установленного логотипа Добавлена проверка изображения на сервере и на ПК, для ускорения работы

--- main.py
+++ main.py
@@ -1,58 +1,77 @@
 import winreg
 import shutil
-from win32com.client import Dispatch
 import platform
 import socket
 import os
 import subprocess
-import csv
+import config
+import filecmp
+import struct
+import ctypes
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from win32com.client import Dispatch
 
-dir = []
-with open('config.txt', 'r', newline='') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        dir.append(row[0])
-
-new_dir = dir[1] # Директория хранения фото на ПК
-auto = dir[2] #автосмена обоев вкл или выкл
-logo = dir[3]
-dir = dir[0] #Директория хранения фото на сервере
+config.local # Директория хранения фото на ПК
+config.auto #автосмена обоев вкл или выкл
+config.logo # Диретория хранения логотипа
+config.server #Директория хранения фото на сервере
 
 #Проверка автоматической смены изображений
 def check_auto_wallpaper():
-    if auto is True:
-        pass
-        #Проверка наличия нового изображения на сервере
-        #ЕСЛИ ЕСТЬ НОВОЕ
-        #Затем копирование нового фото на комп
-        #Отрисовка логотипа
+    print('Проверка автосмены')
+    if config.auto is True:
+        if os.path.isfile(config.server) is True:
+            try:
+                print('Сравнение фото на сервере и на пк')
+                filecmp.cmp(config.server, config.local)
+                print('Файлы одинаковые, замена не требуется')
+            except:
+                print('копирую с сервера')
+                copy_server_to_pc_wallpaper()
+                print('создаю обои')
+                create_image(config.local)
+                pass
+        else:
+            pass
         #Установка обоев
-
-        #ЕСЛИ НЕТ НОВОГО
-        #проверяем логотип
-        #check_logo_weatherford
+        #Запуск функции ожидания 2 часа
     else:
+        print('Проверка лого')
         check_logo_weather()
-        pass
 #Проверка логотипа
 def check_logo_weather(): #проверка логотипа посредством нахождения фото в папке
     aReg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
     aKey = winreg.OpenKey(aReg, r"Control Panel\Desktop")
     keyname = winreg.QueryValueEx(aKey, 'WallPaper')[0]
-    if keyname == new_dir:
+    if keyname == config.new_wallp.replace('/', '\\'):
+        print('лого есть, ждем')
         pass
         #Здесь ссылка на функцию ожидания 2 часа
     else:
-        pass
-        #Здесь функция установки логотипа
-    print(keyname)
+        print('Лого нет, создаем лого')
+        create_image(keyname)
 #Копирование картринки с сервера
 def copy_server_to_pc_wallpaper():
-    shutil.copy(dir, new_dir)
+    print('проверяю доступность сервера и папки на пк')
+    if os.access(config.server, os.R_OK) is True and os.access(config.local, os.W_OK) is True:
+        print('копирую с сервера')
+        shutil.copy(config.server, config.local)
+    else:
+        print('Доступ не получен. Вставляю лого в имеющуюся картинку')
+        wallREG = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+        awallREG = winreg.OpenKey(wallREG, r"Control Panel\Desktop")
+        wallpaper_user = winreg.QueryValueEx(awallREG, 'WallPaper')[0].replace('\\\\', '\\')
+        try:
+            shutil.copy(wallpaper_user, config.local)
+        except:
+            print('файл не найден, копирую новую картинку с сервера')
+            shutil.copy(config.server, config.local)
+'''Запрос информации о ПК'''
 def get_pc_info():
+    print('Запрос информации о ПК')
+    '''Список для параметров'''
     param = []
-
+    '''Запросы из регистра, версии программ и скрипты запроса нужной информации'''
     aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
     aKey = winreg.OpenKey(aReg, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion")
     try:
@@ -64,59 +83,72 @@ def get_pc_info():
     OS_Build = winreg.QueryValueEx(aKey, 'DisplayVersion')[0] #OS Build
     Host_Name = platform.node()
     Mother_name = subprocess.check_output("wmic bios get serialnumber").decode().split()[1]
-
-    ver_parser = Dispatch('Scripting.FileSystemObject')
     try:
+        ver_parser = Dispatch('Scripting.FileSystemObject')
         Crowdstrike_version = ver_parser.GetFileVersion('C:\Program Files\CrowdStrike\CSFalconContainer.exe')
     except:
         Crowdstrike_version = 'None'
-    Edge_version = ver_parser.GetFileVersion('C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
-
+    try:
+        Edge_version = ver_parser.GetFileVersion('C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
+    except:
+        Edge_version = 'None'
     OS_Version = platform.platform(terse=True)
-    IP_adress = socket.gethostbyname(Host_Name)
-    Machine_Domain = socket.getfqdn().split('.', 1)[1]
-    if Machine_Domain == 'wft.root.loc':
-        Machine_Domain = 'WFT'
-    Logon_Domain = os.environ['userdomain']
-    #Logon_Server = subprocess.run("gpresult /r", encoding='utf-8')
+    try:
+        IP_adress = socket.gethostbyname(Host_Name)
+    except:
+        IP_adress = 'None'
+    try:
+        Machine_Domain = socket.getfqdn().split('.', 1)[1]
+        if Machine_Domain == 'wft.root.loc':
+            Machine_Domain = 'WFT'
+        Logon_Domain = os.environ['userdomain']
+    except:
+        Machine_Domain = 'None'
     try:
         Logon_Server = subprocess.check_output('nltest /dsgetdc:wft.root.loc').decode('cp866').split()[2].split('.')[0].replace('\\', '')
     except:
         Logon_Server = 'None'
-    User_name = subprocess.check_output('whoami').decode('cp866').replace('\n','')
+    User_name = subprocess.check_output('whoami').decode('cp866').replace('\n', '')
     if User_name[0:3] == 'wft':
         User_name = User_name[4:]
-
-    param.extend([Host_Name, Mother_name, IP_adress, Machine_Domain,Logon_Domain, Logon_Server, User_name[:-1], OS_Version, Build_number, OS_Build, SCCM_version,Edge_version, Crowdstrike_version])
+        if User_name[0] == 'e':
+            User_name = User_name[0:7]
+    param.extend([Host_Name, Mother_name, IP_adress, Machine_Domain,Logon_Domain, Logon_Server, User_name, OS_Version, Build_number, OS_Build, SCCM_version,Edge_version, Crowdstrike_version])
     return param
-
-def create_image():
-    general_watermark = Image.new('RGB', (300, 440))
+'''Создание картинки с Watermark'''
+def create_image(dir_walpp):
+    print('Создаем изображение')
+    general_watermark = Image.new('RGBA', (300, 370))
     draw_text = ImageDraw.Draw(general_watermark)
     name_param = ['Host Name', 'Serial number', 'IP Adress', 'Machine Domain', 'Logon Domain', 'Logon Server', 'User Name', 'OS Version', 'Build Number', 'OS Build', 'SCCM Client Version', 'Edge Version', 'Crowdstrike Version']
     value_param = get_pc_info()
-    font = ImageFont.truetype('arial.ttf', size=13)
+    font = ImageFont.truetype('arial.ttf', size=15)
     coordy = 170
     i = 0
     for item_param in name_param:
-        draw_text.text((10, coordy), item_param, font=font)
-        draw_text.text((290, coordy + 13), value_param[i], font=font, anchor='rs')
-        coordy += 20
+        draw_text.text((10, coordy), item_param, font=font, stroke_width=1, stroke_fill="black")
+        draw_text.text((290, coordy + 13), value_param[i], font=font, anchor='rs', stroke_width=1, stroke_fill="black")
+        coordy += 15
         i += 1
-
-    watermark = Image.open(logo)
-
-    general_watermark.paste(watermark, (10, 10))
-    #general_watermark.show()
-
-    general_watermark.putalpha(128)
+    watermark = Image.open(config.logo)
+    general_watermark.paste(watermark, (8, 8), mask=watermark.convert('RGBA'))
     copy_server_to_pc_wallpaper()
-    new_wallpapper = Image.open(new_dir)
+    new_wallpapper = Image.open(dir_walpp)
+    new_wallpapper.convert('RGBA')
     position = (new_wallpapper.width - general_watermark.width,
                 new_wallpapper.height - general_watermark.height)
+    new_wallpapper.paste(general_watermark, position, mask=general_watermark.convert('RGBA'))
+    new_wallpapper.save(config.new_wallp)
+    changeBG()
 
-    new_wallpapper.paste(general_watermark, position)
-    new_wallpapper.show()
+def changeBG():
+    """Change background depending on bit size"""
+    bit64 = struct.calcsize('P') * 8 == 64
+    SPI_SETDESKWALLPAPER = 20
+    if bit64 is True:
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, config.new_wallp, 3)
+    else:
+        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, config.new_wallp, 3)
+    print('Обои установлены')
 
-
-create_image()
+check_auto_wallpaper()
