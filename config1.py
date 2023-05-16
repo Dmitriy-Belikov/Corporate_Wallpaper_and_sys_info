928b97febae621ece86b4f80f23da8c2c2032f5f
Dmitriy-Belikov <107193589+Dmitriy-Belikov@users.noreply.github.com>
2023-05-16 19:53:57 +0500

    Delete config1.py

--- config1.py
+++
@@ -1,6 +0,0 @@
-server = 'C:/CSV/wallpaper.jpg'
-local = 'C:/CSV/new/wallpaper.jpg'
-auto = True
-logo = 'C:/CSV/logo.png'
-new_wallp = 'C:/CSV/new/corp_wallpaper.jpg'
-server = 'C:/CSV/wallpaper.jpg'local = 'C:/CSV/new/wallpaper.jpg'auto = Truelogo = 'C:/CSV/logo.png'new_wallp = 'C:/CSV/new/corp_wallpaper.jpg'
